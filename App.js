7c5e7a0403f737524e087530ea3637b7fbfa7170
Fadwa Ali <fadwa.ali@elevoro.com>
2023-10-16 18:12:57 +0300

    create server

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 console.log("NodeJS");
+const { Console } = require("console");
 //core module
 const http = require("http");
-// custom module 
-const {log} = require("./logger");
-log();
-// Third pary module <packages>
+const server =http.createServer();
+const PORT= 5000;
+server.listen(PORT,()=> console.log(`Server is running on port ${PORT}`));
+// // custom module 
+// const {log} = require("./logger");
+// log();
+// // Third pary module <packages>
 
-const express = require("express");
+// const express = require("express");
