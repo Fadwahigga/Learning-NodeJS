fa67ae706a0461a03b3479d521fb27019ce2e916
Fadwa Ali <fadwa.ali@elevoro.com>
2023-10-16 17:44:45 +0300

    to use functions on file

--- App.js
+++ App.js
@@ -2,5 +2,6 @@ console.log("NodeJS");
 //core module
 const http = require("http");
 // custom module 
-const logger = require("./logger");
+const {log} = require("./logger");
 // Third pary module <packages>
+log();
