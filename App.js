0dfaa31677300d6d3c61a47a8d08b1ec1b698b4a
Fadwa Ali <fadwa.ali@elevoro.com>
2023-10-16 18:53:06 +0300

    test get

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
-console.log("NodeJS");
-const { Console } = require("console");
-//core module
-const http = require("http");
-const books = [
-  { id: 1, name: "book1" },
-  { id: 1, name: "book1" },
-];
-const server = http.createServer((request, response) => {
-  if (request.url === "/") {
-    response.write("<h1>Hello<h1>");
-    response.end();
-  }
-  if (request.url === "/api/books") {
-    response.write(JSON.stringify(books));
-    response.end();
-  }
+// console.log("NodeJS");
+// const { Console } = require("console");
+// //core module
+// const http = require("http");
+const express = require("express");
+const app = express();
+app.get("/",(req,res)=>{
+  res.send("Helllll");
 });
+const books = [
+  { id: 1, name: "book1", tittle: "neww", author: "fadwa" },
+  { id: 2, name: "book2", tittle: "newwww", author: "fadwwwa" },
+  { id: 3, name: "book3", tittle: "newew", author: "fadwaaa" },
+];
+// const server = http.createServer((request, response) => {
+//   if (request.url === "/") {
+//     response.write("<h1>Hello<h1>");
+//     response.end();
+//   }
+//   if (request.url === "/api/books") {
+//     response.write(JSON.stringify(books));
+//     response.end();
+//   }
+// });
 const PORT = 5000;
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 // // custom module
 // const {log} = require("./logger");
 // log();
 // // Third pary module <packages>
 
-// const express = require("express");
+
