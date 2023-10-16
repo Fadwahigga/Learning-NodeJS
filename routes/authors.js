c97e800eb24bec30386c2445e292480c50143f03
Fadwa Ali <fadwa.ali@elevoro.com>
2023-10-16 23:15:37 +0300

    delete author

--- routes/authors.js
+++ routes/authors.js
@@ -54,7 +54,7 @@ router.get("/:id",(req,res)=>{
  * @access public
  */
 router.post("/", (req, res) => {
-    const { error } = ValidateAddBook(req.body);
+    const { error } = ValidateAddAuthor(req.body);
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
     }
@@ -70,15 +70,58 @@ router.post("/", (req, res) => {
     res.status(201).json(author); //201 created successfully
   });
 
+/**
+ * @des Udpate  Author
+ * @route /author/:id
+ * @method PUT
+ * @access public
+ */
+router.put("/:id", (req, res) => {
+    const { error } = ValidateUpdatAuthor(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    const author = authors.find((b) => b.id === parseInt(req.params.id));
+    if (author) {
+      res.status(200).json({ menubar: "Author has been updated" });
+    } else {
+      res.status(404).json({ message: "Author not found" });
+    }
+  });
 
+  /**
+ * @des Delete Author
+ * @route /author/:id
+ * @method Delete
+ * @access public
+ */
+router.delete("/:id", (req, res) => {
+
+    const author = authors.find((b) => b.id === parseInt(req.params.id));
+    if (author) {
+      res.status(200).json({ menubar: "Author has been deleted" });
+    } else {
+      res.status(404).json({ message: "Author not found" });
+    }
+  });
 
 // Validate Add Author
-  function  ValidateAddBook(obj) {
+  function  ValidateAddAuthor(obj) {
     const schema = Joi.object({
       first_name: Joi.string().trim().min(3).max(100).required(),
       last_name: Joi.string().trim().min(3).max(100).required(),
       nationality: Joi.string().trim().min(3).max(100).required(),
       image: Joi.string().required(),
+    });
+    return schema.validate(obj);
+  }
+  // Validate Update Author
+  function  ValidateUpdatAuthor(obj) {
+    const schema = Joi.object({
+      first_name: Joi.string().trim().min(3).max(100),
+      last_name: Joi.string().trim().min(3).max(100),
+      nationality: Joi.string().trim().min(3).max(100),
+      image: Joi.string(),
     });
     return schema.validate(obj);
   }
