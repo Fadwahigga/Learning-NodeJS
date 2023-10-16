34149bcdbbba3255073764780d8f4df8f884b950
Fadwa Ali <fadwa.ali@elevoro.com>
2023-10-16 20:45:25 +0300

    Input Validation

--- App.js
+++ App.js
@@ -4,13 +4,13 @@
 // const http = require("http");
 const express = require("express");
 const app = express();
-const Joi = require('joi');
+const Joi = require("joi");
 // Add middlewares
 app.use(express.json());
 const books = [
-  { id: 1, name: "book1", tittle: "neww", author: "fadwa" },
-  { id: 2, name: "book2", tittle: "newwww", author: "fadwwwa" },
-  { id: 3, name: "book3", tittle: "newew", author: "fadwaaa" },
+  { id: 1, name: "book1", tittle: "neww", author: "fadwa", price: "203" },
+  { id: 2, name: "book2", tittle: "newwww", author: "fadwwwa", price: "200" },
+  { id: 3, name: "book3", tittle: "newew", author: "fadwaaa", price: "20" },
 ];
 app.get("/", (req, res) => {
   res.send("Hi from nodemon");
@@ -32,12 +32,22 @@ app.get("/books/:id", (req, res) => {
 
 // Add new book
 app.post("/books", (req, res) => {
-
+  const schema = Joi.object({
+    name: Joi.string().trim().min(3).max(100).required(),
+    tittle: Joi.string().trim().min(3).max(100).required(),
+    author: Joi.string().trim().min(3).max(100).required(),
+    price: Joi.number().min(0).required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
   const book = {
     id: books.length + 1,
     name: req.body.name,
     tittle: req.body.tittle,
     author: req.body.author,
+    price: req.body.price,
   };
   console.log(book);
   books.push(book);
