0fdef8c90f7e27debb2d24bca588776823f9cf03
Fadwa Ali <fadwa.ali@elevoro.com>
2023-10-20 16:44:49 +0300

    add model

--- models/Authors.js
+++ models/Authors.js
@@ -1,34 +1,36 @@
-const { string } = require("joi");
+
 const mongoose = require("mongoose");
-const AuthorSchema = new mongoose.Schema({
-  first_name: {
-    type: string,
-    required: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 100,
+const AuthorSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    nationality: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    image: {
+      type: String,
+      default: "defult.png",
+    },
   },
-  last_name: {
-    type: string,
-    required: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 100,
-  },
-  nationality: {
-    type: string,
-    required: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 100,
-  },
-  image: {
-    type: string,
-    default: "defult.png",
-  },
-  timestamps: true,
-});
+  { timestamps: true }
+);
 const Author = mongoose.model("Author", AuthorSchema);
-model.export={
-    Author
-}
+module.exports = {
+  Author,
+};
--- routes/authors.js
+++ routes/authors.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const Joi = require("joi");
 const router = express.Router();
+const { Author } = require("../models/Authors");
 const authors = [
   {
     id: 1,
-    first_name: "fadwa",
-    last_name: "ali",
+    firstName: "fadwa",
+    lastName: "ali",
     nationality: "sudan",
     image: "default.png",
   },
@@ -39,36 +40,41 @@ router.get("/", (req, res) => {
  * @method Get
  * @access public
  */
-router.get("/:id",(req,res)=>{
-    const author = authors.find((a)=> a.id === parseInt(req.params.id));
-    if(author){
-        res.status(200).json(author);
-    }else{
-        res.status(404).json({message:"Author not found"});
-    }
-})
+router.get("/:id", (req, res) => {
+  const author = authors.find((a) => a.id === parseInt(req.params.id));
+  if (author) {
+    res.status(200).json(author);
+  } else {
+    res.status(404).json({ message: "Author not found" });
+  }
+});
 /**
  * @des Add New Author
  * @route /author
  * @method Post
  * @access public
  */
-router.post("/", (req, res) => {
-    const { error } = ValidateAddAuthor(req.body);
-    if (error) {
-      return res.status(400).json({ message: error.details[0].message });
-    }
-    const author = {
-        id: authors.length +1,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        nationality: req.body.nationality,
-        image: req.body.image,
-    };
-    console.log(author);
-    authors.push(author);
-    res.status(201).json(author); //201 created successfully
-  });
+router.post("/", async (req, res) => {
+  const { error } = ValidateAddAuthor(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  try {
+    const author = new Author({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      nationality: req.body.nationality,
+      image: req.body.image,
+    });
+    const result = await author.save();
+    res.status(201).json(author);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something want wrong" });
+  }
+
+  //201 created successfully
+});
 
 /**
  * @des Udpate  Author
@@ -77,52 +83,51 @@ router.post("/", (req, res) => {
  * @access public
  */
 router.put("/:id", (req, res) => {
-    const { error } = ValidateUpdatAuthor(req.body);
-    if (error) {
-      return res.status(400).json({ message: error.details[0].message });
-    }
-    const author = authors.find((b) => b.id === parseInt(req.params.id));
-    if (author) {
-      res.status(200).json({ menubar: "Author has been updated" });
-    } else {
-      res.status(404).json({ message: "Author not found" });
-    }
-  });
+  const { error } = ValidateUpdatAuthor(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  const author = authors.find((b) => b.id === parseInt(req.params.id));
+  if (author) {
+    res.status(200).json({ menubar: "Author has been updated" });
+  } else {
+    res.status(404).json({ message: "Author not found" });
+  }
+});
 
-  /**
+/**
  * @des Delete Author
  * @route /author/:id
  * @method Delete
  * @access public
  */
 router.delete("/:id", (req, res) => {
-
-    const author = authors.find((b) => b.id === parseInt(req.params.id));
-    if (author) {
-      res.status(200).json({ menubar: "Author has been deleted" });
-    } else {
-      res.status(404).json({ message: "Author not found" });
-    }
-  });
+  const author = authors.find((b) => b.id === parseInt(req.params.id));
+  if (author) {
+    res.status(200).json({ menubar: "Author has been deleted" });
+  } else {
+    res.status(404).json({ message: "Author not found" });
+  }
+});
 
 // Validate Add Author
-  function  ValidateAddAuthor(obj) {
-    const schema = Joi.object({
-      first_name: Joi.string().trim().min(3).max(100).required(),
-      last_name: Joi.string().trim().min(3).max(100).required(),
-      nationality: Joi.string().trim().min(3).max(100).required(),
-      image: Joi.string().required(),
-    });
-    return schema.validate(obj);
-  }
-  // Validate Update Author
-  function  ValidateUpdatAuthor(obj) {
-    const schema = Joi.object({
-      first_name: Joi.string().trim().min(3).max(100),
-      last_name: Joi.string().trim().min(3).max(100),
-      nationality: Joi.string().trim().min(3).max(100),
-      image: Joi.string(),
-    });
-    return schema.validate(obj);
-  }
+function ValidateAddAuthor(obj) {
+  const schema = Joi.object({
+    firstName: Joi.string().trim().min(3).max(100).required(),
+    lastName: Joi.string().trim().min(3).max(100).required(),
+    nationality: Joi.string().trim().min(3).max(100).required(),
+    image: Joi.string().required(),
+  });
+  return schema.validate(obj);
+}
+// Validate Update Author
+function ValidateUpdatAuthor(obj) {
+  const schema = Joi.object({
+    firstName: Joi.string().trim().min(3).max(100),
+    lastName: Joi.string().trim().min(3).max(100),
+    nationality: Joi.string().trim().min(3).max(100),
+    image: Joi.string(),
+  });
+  return schema.validate(obj);
+}
 module.exports = router;
