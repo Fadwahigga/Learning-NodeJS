5294a07f586363079cca45b29f437e83bcddbf9e
Fadwa Ali <fadwa.ali@elevoro.com>
2023-10-21 17:27:08 +0300

    add ent config

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ const express = require("express");
 const booksPath = require("./routes/books");
 const authorsPath = require("./routes/authors");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 // Conection to database
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
