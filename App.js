7f940ec1f65eddd4d762adb683291c1dcf831f6f
Fadwa Ali <fadwa.ali@elevoro.com>
2023-10-21 17:36:27 +0300

    fix env route

--- App.js
+++ App.js
@@ -4,9 +4,14 @@ const authorsPath = require("./routes/authors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+// Check if the environment variable is loaded
+if (!process.env.MONGO_URI) {
+  console.error("MongoDB URI is missing in your .env file or environment variables.");
+  process.exit(1);
+}
 // Conection to database
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to the database"))
   .catch((error) => console.log("Failed to connect to the database", error));
 
@@ -21,26 +26,5 @@ app.use("/books", booksPath);
 app.use("/authors", authorsPath);
 
 // Running the server
-const PORT = 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-//////////////////////////////////////////////////////////////////////////////
-// console.log("NodeJS");
-// const { Console } = require("console");
-// //core module
-// const http = require("http");
-// const server = http.createServer((request, response) => {
-//   if (request.url === "/") {
-//     response.write("<h1>Hello<h1>");
-//     response.end();
-//   }
-//   if (request.url === "/api/books") {
-//     response.write(JSON.stringify(books));
-//     response.end();
-//   }
-// });
-// // custom module
-// const {log} = require("./logger");
-// log();
-// // Third pary module <packages>
-/////////////////////////////////////////////////////////////////////////////
