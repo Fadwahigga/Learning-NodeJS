35e7f7e89eab2f1cfd8f8c5d13f29398eff798b1
Fadwa Ali <fadwa.ali@elevoro.com>
2023-10-21 17:07:53 +0300

    create book router

--- routes/books.js
+++ routes/books.js
@@ -31,12 +31,17 @@ router.get("/", async (req, res) => {
  * @method Get
  * @access public
  */
-router.get("/:id", (req, res) => {
-  const book = books.find((b) => b.id === parseInt(req.params.id));
-  if (book) {
-    res.status(200).json(book);
-  } else {
-    res.status(404).json({ message: "Book not found" });
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (book) {
+      res.status(200).json(book);
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 /**
@@ -45,21 +50,27 @@ router.get("/:id", (req, res) => {
  * @method Post
  * @access public
  */
-router.post("/", (req, res) => {
-  const { error } = validateCreateBook(req.body);
+router.post("/", async (req, res) => {
+  const { error } = ValidateAddBook(req.body);
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
-  const book = {
-    id: books.length + 1,
-    name: req.body.name,
-    tittle: req.body.tittle,
-    author: req.body.author,
-    price: req.body.price,
-  };
-  console.log(book);
-  books.push(book);
-  res.status(201).json(book); //201 created successfully
+  try {
+    const book = new Book({
+      tittle: req.body.tittle,
+      author: req.body.author,
+      description: req.body.description,
+      price: req.body.price,
+      cover: req.body.cover
+    });
+    const result = await book.save();
+    res.status(201).json(book);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something want wrong" });
+  }
+
+  //201 created successfully
 });
 
 /**
@@ -68,17 +79,25 @@ router.post("/", (req, res) => {
  * @method PUT
  * @access public
  */
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   const { error } = validateUpdateBook(req.body);
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
-  const book = books.find((b) => b.id === parseInt(req.params.id));
-  if (book) {
-    res.status(200).json({ menubar: "Book has been updated" });
-  } else {
-    res.status(404).json({ message: "Book not found" });
-  }
+  const book = await Book.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        tittle: req.body.tittle,
+        author: req.body.author,
+        description: req.body.description,
+        price: req.body.price,
+        cover: req.body.cover,
+      },
+    },
+    { new: true }
+  );
+  res.status(200).json(book);
 });
 /**
  * @des Delete a Book
@@ -86,12 +105,18 @@ router.put("/:id", (req, res) => {
  * @method Delete
  * @access public
  */
-router.delete("/:id", (req, res) => {
-  const book = books.find((b) => b.id === parseInt(req.params.id));
-  if (book) {
-    res.status(200).json({ menubar: "Book has been deleted" });
-  } else {
-    res.status(404).json({ message: "Book not found" });
+router.delete("/:id", async (req, res) => {
+  try {
+    const book = Book.findById(req.params.id);
+    if (book) {
+      await Book.findByIdAndDelete(req.params.id);
+      res.status(200).json({ menubar: "Book has been deleted" });
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something want wrong" });
   }
 });
 
